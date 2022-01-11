af1805e77dd49b48dc0e6f6b4bdd5d2eb91cd8ab
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-11 16:46:13 +0700

    connect to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+// options object is optional, here is just to remove the deprecation warnings
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => console.log("SUCCESSFULLY CONNECTED TO MONGODB!!!!"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
