0555a1b21dc41c70d5b53ca7bbf8fcf1312fc622
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-13 11:11:09 +0700

    handling unhandled routes

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// Log "API IS CALLED" everytime API is called
-app.use((req, res, next) => {
-  console.log("API IS CALLED");
-  next();
-});
-
 // Add request time to request object
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -27,5 +21,14 @@ app.use((req, res, next) => {
 // 2) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+// Handler for all other routes that are not supported, this has to come last after all middleware so that no defined route is left
+app.all("*", (req, res, next) => {
+  // .all() is for all verbs (GET, POST,...)
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
--- models/tourModel.js
+++ models/tourModel.js
@@ -124,7 +124,7 @@ tourSchema.pre(/^find/, function(next) {
 });
 
 tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query tool ${Date.now() - this.start}ms!`);
+  console.log(`Query took ${Date.now() - this.start}ms!`);
   next();
 });
 
