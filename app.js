5bd529743638e1ef0942431637b7db95e7678798
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-16 10:36:10 +0700

    NoSQL injection and XSS data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -9,8 +12,6 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
-
-const helmet = require("helmet");
 
 // Set security HTTP headers
 app.use(helmet());
@@ -31,6 +32,12 @@ app.use("/api", limiter);
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize()); // basically filter out '$' and '.' in query
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
