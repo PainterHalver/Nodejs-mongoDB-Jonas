2ea23c2bf1d83853f1f364da90376eef1af3ec32
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-16 10:19:44 +0700

    Implemented Rate Limiting

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Notes:
 
 - mongoose can slow down vscode's tsserver.js (high cpu, slow code autocomplete) (https://github.com/Automattic/mongoose/issues/10349)
+- Not a good practice to upload config file anywhere but this project is for learning so :)
--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// "rateLimit" returns a middleware function
+const limiter = rateLimit({
+  max: 100, // allow maximum of 100 requests from 1 ip
+  windowMs: 60 * 60 * 1000, // in 1 hour
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
