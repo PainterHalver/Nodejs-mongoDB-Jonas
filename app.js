28fa189ffbe2e76ef4e09123bf4ef3b6d7f13351
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-18 18:31:56 +0700

    setting up project structure for pug

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const viewRouter = require("./routes/viewRoutes");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
@@ -69,10 +70,7 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base");
-});
-
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
