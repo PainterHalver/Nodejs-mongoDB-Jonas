b47e1e6ca5dd9e725eb74ca1ba9418c858a7fc32
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-18 10:51:45 +0700

    calculating average rating on tours from reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -40,6 +41,56 @@ reviewSchema.pre(/^find/, function(next) {
   });
 
   next();
+});
+
+// static methods
+reviewSchema.statics.calculateAverageRatings = async function(tourId) {
+  // 'this' now points to the Model, that's why we need a static method
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+// we want to calculate after saving not before so "post"
+reviewSchema.post("save", function() {
+  // 'this' points to current review document
+  this.constructor.calculateAverageRatings(this.tour);
+});
+
+// findByIdAndUpdate calls findOneAndUpdate
+// findByIdAndDelete calls findOneAndDelete
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  // 'this' is the query
+  // now we need the tourId and somehow pass it to the post middleware
+  this.review = await this.findOne();
+  next();
+});
+
+// and we want to calculate after review is updated not before (tricky)
+reviewSchema.post(/^findOneAnd/, async function(next) {
+  // await this.findOne() does not work here because query has already executed
+  await this.review.constructor.calculateAverageRatings(this.review.tour);
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
