bb0cc80ba3a1e93ff2c9273e82dfde1a271f3598
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-19 18:31:52 +0700

    image processing (resizing) with 'sharp'

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,12 @@
 const multer = require("multer");
+const sharp = require("sharp");
 const AppError = require("../utils/appError");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const factory = require("./handlerFactory");
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "public/img/users");
-  },
-  filename: (req, file, callback) => {
-    // user-7451as41adavas4-51351531513.jpeg for unique
-    const extension = file.mimetype.split("/")[1];
-    callback(null, `user-${req.user.id}-${Date.now()}.${extension}`);
-  },
-});
+// store img as buffer
+const multerStorage = multer.memoryStorage();
 
 // filter image (make sure it is an image)
 const multerFilter = (req, file, callback) => {
@@ -33,6 +26,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
