24721f5b3abaeaa5f6e03fbe8fa28e2b22b8ad78
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-20 14:21:04 +0700

    added map with mapbox

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -155,3 +155,33 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/233/center/21.0385921,105.7852037/unit/mi
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(",");
+
+  const radius = unit === "mi" ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        "Please provide latitutr and longitude in the format lat,lng.",
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      data: tours,
+    },
+  });
+});
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,9 +1,50 @@
+import mapboxgl from "mapbox-gl";
+
 export const displayMap = (locations) => {
   mapboxgl.accessToken =
     "pk.eyJ1IjoiY2FscHV0ZXIiLCJhIjoiY2t5a3dxbDJxMzBneTJ2bjAxOTRsNXZmayJ9.R89TGMjEtQ_TqSXA4aMx-Q";
 
   var map = new mapboxgl.Map({
     container: "map", // put the map on element with id of 'map'
-    style: "mapbox://styles/mapbox/streets-v11",
+    style: "mapbox://styles/calputer/ckymmgj5092mo14pp2w7qn5ba",
+    scrollZoom: false,
+    // center: [105.7852037, 21.0385921], // [long, lat]
+    // zoom: 15,
+    // interactive: false,
+  });
+
+  const bounds = new mapboxgl.LngLatBounds();
+
+  locations.forEach((loc) => {
+    // Add marker
+    const el = document.createElement("div");
+    el.className = "marker";
+
+    new mapboxgl.Marker({
+      Element: el,
+      anchor: "bottom",
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
+
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
+
+    bounds.extend(loc.coordinates);
+  });
+
+  // Nice zoom in animation
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
   });
 };
