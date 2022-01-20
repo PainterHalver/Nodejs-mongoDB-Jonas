08a6c696733bc7b38f07a6505e17b309f9c9f54a
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-20 16:53:40 +0700

    Prepared app for deployment

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -76,6 +77,9 @@ app.use(
     ],
   })
 );
+
+// Compression middleware (compress texts)
+app.use(compression());
 
 // Add request time to request object
 app.use((req, res, next) => {
